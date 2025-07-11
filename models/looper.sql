436e339f5d73b964cfef699b308a6ce2a6044bd0
Anders Swanson <anders.swanson@dbtlabs.com>
2025-07-11 11:22:25 -0400

    repro

---
+++ models/looper.sql
@@ -0,0 +1,15 @@
+{{ config(static_analysis='unsafe') }}
+
+{% set sku_list = dbt_utils.get_column_values(
+            table=ref('random_skus'),
+            column='sku'
+            ) -%}
+        {%- for sku in sku_list %}
+            {%set sku = sku | replace("SKU","") %}
+                select
+                    {{ sku }} as sku
+                from {{ ref('random_skus') }}
+            {%- if not loop.last %}
+                union all
+            {%- endif -%}
+        {% endfor %}
