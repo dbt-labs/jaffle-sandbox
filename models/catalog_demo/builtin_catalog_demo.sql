3ac0f6681f3d48dc022d1f7c07ce0f368da9a96d
Anders Swanson <anders.swanson@dbtlabs.com>
2026-05-25 17:09:55 -0400

    Remove obsolete catalog demo artifacts

--- models/catalog_demo/builtin_catalog_demo.sql
+++
@@ -1,9 +0,0 @@
-{{ config(
-    materialized='table',
-    tags=['catalog_demo', 'capability_probe', 'catalog_builtin', 'catalog_writable_expected']
-) }}
-
-select
-  1 as demo_order,
-  'builtin' as catalog_name,
-  'local duckdb file' as storage_type
